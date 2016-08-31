e8b837b296d18508c8f76bfaf09fa11b05912e76
Rasmus Villemoes <rasmus.villemoes@prevas.dk>
2016-08-31 13:05:43 +0200

    classes/package.oeclass: do fewer stat calls during do_split
    We do 100000s of stat system calls, and while the linux kernel's
    dcache makes that reasonably inexpensive, it's still a lot of useless
    context switching and path walking.
    
    This implements a simple stat result cacher, which can be used for the
    most common queries (isdir, islink, isfile, exists, lexists), as well
    as receiving the stat result itself (False if the file didn't
    exist). It is obviously only valid as long as the user knows that the
    caller doesn't do anything that might invalidate it, but that's
    relatively easy to verify in the tight inner loops where this will be
    most useful.

--- lib/oelite/path.py
+++ lib/oelite/path.py
@@ -1,4 +1,5 @@
 import os
+from stat import *
 
 TOPDIR = os.getcwd()
 
@@ -25,3 +26,60 @@ def which(path, filename, pathsep=os.pathsep):
         if os.path.exists(f):
             return os.path.abspath(f)
     return '' # TODO: change to None, and fixup the breakage it causes
+
+class StatCache:
+    def __init__(self, fn):
+        self.fn = fn
+        self._lstat = None
+        self._stat = None
+
+    # os.path.is* and os.path.[l]exists ignore all OSError exceptions,
+    # not just ENOENT, so we do as well.
+    def lstat(self):
+        if self._lstat is None:
+            try:
+                self._lstat = os.lstat(self.fn)
+            except OSError:
+                self._lstat = False
+        return self._lstat
+
+    def stat(self):
+        if self._stat is None:
+            # If we've done an lstat and that didn't find a file, stat() won't either.
+            if self._lstat is False:
+                self._stat = False
+            # If we've done a succesful lstat and that found a
+            # non-link, we can reuse the stat structure.
+            elif self._lstat is not None and not S_ISLNK(self._lstat.st_mode):
+                self._stat = self._lstat
+            else:
+                # OK, either we haven't done an lstat or the file is a link. Gotta do stat.
+                try:
+                    self._stat = os.stat(self.fn)
+                except OSError:
+                    self._stat = False
+        return self._stat
+
+    def _answer(self, statfunc, predicatefunc):
+        s = statfunc()
+        if s is False:
+            return False
+        return predicatefunc(s.st_mode)
+
+    def islink(self):
+        return self._answer(self.lstat, S_ISLNK)
+
+    def isdir(self):
+        return self._answer(self.stat, S_ISDIR)
+
+    def isfile(self):
+        return self._answer(self.stat, S_ISREG)
+
+    def exists(self):
+        return self._answer(self.stat, lambda x: True)
+
+    def lexists(self):
+        return self._answer(self.lstat, lambda x: True)
+
+def statcache(fn):
+    return StatCache(fn)
