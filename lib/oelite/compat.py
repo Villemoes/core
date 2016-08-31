977e7892a5e8155e123f6bfe8ccc0c91eec39912
Rasmus Villemoes <rasmus.villemoes@prevas.dk>
2016-08-31 13:05:47 +0200

    oelite/compat.py: implement sendfile shim
    Modern linux has sendfile for doing efficient copying between two file
    descriptors without copying the data to and from userspace. That's not
    exposed through the Python os module before 3.3. For now, create a
    fallback implementation.
    
    Later, we can use ctypes to access the actual libc wrapper and hence
    the actual system call.
    
    This is also a first stab at trying to introduce formal
    unittests. sendfile itself is a bit hard to test, so I also
    implemented the user I actually have in mind, copyfile.

--- lib/oelite/compat.py
+++ lib/oelite/compat.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 import os
+import errno
 import fcntl
 import ctypes
 import datetime
 import sys
 
-__all__ = ["dup_cloexec", "open_cloexec"]
+__all__ = ["dup_cloexec", "open_cloexec", "copyfile"]
 
 uname = os.uname()
 sysname, release, machine = (uname[0], uname[2], uname[4])
@@ -59,6 +60,69 @@ else:
         dup_cloexec = dup_cloexec_fallback
 
 
+# os.sendfile exists from Python 3.3, but the "offset" argument seems
+# to be handled a little weird (it certainly cannot behave as the
+# underlying system call), and we don't really need it anyway. Also,
+# implementing a fallback supporting it is prohibitively hard since
+# os.pread also doesn't exist until 3.3.
+def sendfile_fallback(dfd, sfd, count):
+    buf = os.read(sfd, count)
+    count = len(buf)
+    if count == 0:
+        return 0
+    written = 0
+    try:
+        # Attempt a single write of the whole buffer first, to avoid
+        # Python creating a copy of the buffer before passing it to
+        # os.write, as it probably will inside the loop.
+        written += os.write(dfd, buf)
+        while written < count:
+            written += os.write(dfd, buf[written:])
+        return written
+    except Exception as e:
+        # If something went wrong writing the buffer, try to reset the
+        # input file descriptor so the data can be read again.
+        try:
+            os.lseek(sfd, -(count - written), os.SEEK_CUR)
+        except:
+            pass
+        raise e
+
+# FIXME: Implement a ctypes-based sendfile that will actually use the
+# underlying system call.
+sendfile = sendfile_fallback
+
+def copyfile(src, dst, mode=0666):
+    """Copy a regular file from src to dst. If possible, the atime of src
+    is not modified. If dst exists, it must be a regular file (which
+    will be truncated and overwritten) - a symbolic link is _not_
+    followed. Otherwise, the caller must ensure that dirname(dst)
+    exists. The caller must also ensure that src and dst do not name
+    the same file (otherwise it will be silently truncated). Returns
+    True on success, propagates exception on failure.
+
+    """
+    try:
+        sfd = os.open(src, os.O_RDONLY | os.O_NOATIME)
+    except OSError as e:
+        if e.errno == errno.EPERM:
+            sfd = os.open(src, os.O_RDONLY)
+        else:
+            raise
+    try:
+        dfd = os.open(dst, os.O_WRONLY | os.O_CREAT | os.O_TRUNC | os.O_NOFOLLOW, mode)
+        try:
+            while True:
+                ret = sendfile(dfd, sfd, 1 << 14)
+                if ret == 0:
+                    return True
+        finally:
+            os.close(dfd)
+    finally:
+        os.close(sfd)
+
+# Very simple selftests follow:
+
 def has_cloexec(fd):
     flags = fcntl.fcntl(fd, fcntl.F_GETFD)
     return (flags & fcntl.FD_CLOEXEC) != 0
@@ -76,7 +140,97 @@ def test_dup_cloexec():
     assert(has_cloexec(fd))
     os.close(fd)
 
+def test_copyfile():
+    import tempfile
+    import filecmp
+    import unittest
+    # If you're strace'ing to see what this does, it may be useful to do
+    #
+    #   strace -e 'trace=!read,write' ./compat.py
+    #
+    # to avoid lots of clutter.
+    def create_tmpfile(size):
+        f = tempfile.NamedTemporaryFile(prefix="copytest_%d_" % size, delete=False)
+        name = f.name
+        while size > 0:
+            w = min(size, 1<<14)
+            f.write("x"*w)
+            size -= w
+        f.close()
+        return name
+
+    class CopyTest(unittest.TestCase):
+        def __init__(self, methodName='runTest', **param):
+            super(CopyTest, self).__init__(methodName)
+            self.size = param['size']
+            self.xdev = param['xdev']
+            self.param = param
+        def setUp(self):
+            self.src = create_tmpfile(self.size)
+            self.dst = self.src + ".copy"
+            if self.xdev:
+                # Just use the current working directory - we assume
+                # /tmp and $cwd are on different devices, and that we
+                # can actually create files in $cwd. The generated
+                # file name should be unique enough.
+                self.dst = os.path.basename(self.dst)
+            if os.path.lexists(self.dst):
+                os.unlink(self.src)
+                self.skipTest("target %s already exists!" % self.dst)
+
+        def tearDown(self):
+            os.unlink(self.src)
+            try:
+                os.unlink(self.dst)
+            except OSError:
+                pass
+
+        def do_copy(self):
+            return copyfile(self.src, self.dst)
+
+        def check_copy(self):
+            return filecmp.cmp(self.src, self.dst)
+
+        def __str__(self):
+            name = self.__class__.__name__
+            name += ": "
+            name += self._testMethodName.replace("test_", "", 1)
+            for p in sorted(self.param.keys()):
+                name += ", %s=%s" % (p, self.param[p])
+            return name
+
+        def test_basic(self):
+            self.assertTrue(self.do_copy())
+            self.assertTrue(self.check_copy())
+
+        def test_dst_exists(self):
+            # Just do the basic test twice, without teardown
+            self.test_basic()
+            self.test_basic()
+
+        def test_src_unreadable(self):
+            os.chmod(self.src, 0)
+            with self.assertRaises(OSError) as cm:
+                self.do_copy()
+            self.assertEqual(cm.exception.errno, errno.EACCES)
+
+        def test_dst_dir_missing(self):
+            self.dst = "/this/path/should/not/exist"
+            with self.assertRaises(OSError) as cm:
+                self.do_copy()
+            self.assertEqual(cm.exception.errno, errno.ENOENT)
+
+    suite = unittest.TestSuite()
+    for size in (0, 1, 10, 100, 1000, 10000, 100*1000, 1000*1000):
+        for method in (unittest.defaultTestLoader.getTestCaseNames(CopyTest)):
+            test = CopyTest(methodName=method, size=size, xdev = False)
+            suite.addTest(test)
+            test = CopyTest(methodName=method, size=size, xdev = True)
+            suite.addTest(test)
+    runner = unittest.TextTestRunner(verbosity=3)
+    runner.run(suite)
 
 if __name__ == "__main__":
     test_open_cloexec()
     test_dup_cloexec()
+    test_copyfile()
