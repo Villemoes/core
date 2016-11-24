739138e340d1e17551631f1e6b812f6ee289742d
Rasmus Villemoes <rv@rasmusvillemoes.dk>
2016-11-24 01:22:18 +0100

    oelite/lockfile.py: rough lockfile implementation
    If we want to do some mutual exclusion in the ingredients dir or
    elsewhere, we will probably need some inter-process mechanism. Locking
    a file using flock() is probably the simplest, and has the important
    property that the necessary OS handles are actually exposed in Python
    2.7.
    
    I've tried to bolt a timeout mechanism on top, which we most likely
    want. It's all entirely untested.

---
+++ lib/oelite/lockfile.py
@@ -0,0 +1,128 @@
+import os
+import fcntl
+import errno
+
+from fcntl import LOCK_SH, LOCK_EX, LOCK_NB, LOCK_UN, flock
+
+from .compat import open_cloexec
+
+def _oserror(err, txt):
+    return OSError(err, os.strerror(err), txt)
+
+# This is really a somewhat generic method for implementing timeouts
+# on blocking system calls that don't natively support a timeout
+# parameter. Set an itimer and, if the system call fails with EINTR,
+# check if the timer fired - if so, change EINTR to ETIMEDOUT,
+# otherwise restart the system call.
+#
+# In Python, the easiest way for a signal handler to be able to mutate
+# state is for it to be an instance method.
+#
+# We want to ensure we reset the itimer and the signal handler, so
+# this only supports being used as a context manager.
+#
+# We do not attempt to restore the old value of the ITIMER_REAL
+# timer. Also, nesting these is obviously meaningless; only use them
+# for wrapping a single system call.
+class Timeout(object):
+    def __init__(self, timeout):
+        self.timeout = timeout
+        
+    def handler(self, signum, frame):
+        self.expired = True
+
+    # Py3
+    def __bool__(self):
+        return self.expired
+    # Py2
+    def __nonzero__(self):
+        return self.__bool__()
+
+    def __enter__(self):
+        self.expired = False
+        self.oldhandler = signal.signal(signal.SIGALRM, self.handler)
+        signal.setitimer(signal.ITIMER_REAL, self.timeout)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Disable the timer before restoring the old signal handler -
+        # if we're unlucky and the timer hasn't expired, it could do
+        # so just after restoring the signal handler but before
+        # disabling the timer. Since the default disposition for
+        # SIGALRM (and hence presumably what we're setting the handler
+        # back to) is to terminate the process, that would be a pity.
+        signal.setitimer(signal.ITIMER_REAL, 0)
+        signal.signal(signal.SIGALRM, self.oldhandler)
+        return False
+
+# Even if we create the file during open(), we never attempt to unlink the file.
+class LockFile(object):
+    def __init__(self, name, default_flags = LOCK_EX):
+        self.name = name
+        # The default_flags is for use in a with statement, where we
+        # can't otherwise specify the type of lock we want.
+        self.default_flags = default_flags
+        self.fd = -1
+
+    def open(self):
+        # Create the file if it doesn't exist, don't accept symlinks,
+        flags = os.O_RDWR | os.O_CREAT | os.O_NOFOLLOW
+        mode = 0o644
+        self.fd = open_cloexec(self.name, flags, mode)
+
+    def close(self):
+        os.close(self.fd)
+        self.fd = -1
+
+    def lock(self, flags = None, timeout = None):
+        if flags is None:
+            flags = default_flags
+        # flags must be LOCK_SH or LOCK_EX, possibly ORed with LOCK_NB.
+        if not (flags & ~LOCK_NB) in (LOCK_SH, LOCK_EX):
+            raise _oserror(errno.EINVAL, "invalid flags")
+
+        if timeout is not None and timeout <= 0:
+            flags |= LOCK_NB
+
+        # If the caller requested a non-blocking operation, or
+        # requested an infinitely-blocking operation (by not passing a
+        # timeout), we can simply call flock.
+        if timeout is None or (flags & LOCK_NB):
+            return flock(self.fd, flags)
+
+        assert(timeout > 0)
+        assert((flags & LOCK_NB) == 0)
+
+        # Even if the user didn't pass LOCK_NB, we do one "trylock"
+        # operation, so that we avoid mucking with signals in the
+        # presumably very common case where we can get the lock
+        # immediately.
+        try:
+            return flock(self.fd, flags | LOCK_NB)
+        except OSError as e:
+            pass
+
+        with Timeout(timeout) as expired:
+            while True:
+                try:
+                    return flock(self.fd, flags)
+                except OSError as e:
+                    if e.errno == errno.EINTR:
+                        if expired:
+                            raise _oserror(errno.ETIMEDOUT, "timeout waiting for lock")
+                        continue
+                    raise
+
+    def unlock(self):
+        return flock(self.fd, LOCK_UN)
+
+    def __enter__(self):
+        self.open()
+        return self.lock() # returns None or raises exception
+
+    def __exit__(self):
+        self.unlock()
+        self.close()
+
+if __name__ == "__main__":
+    pass
