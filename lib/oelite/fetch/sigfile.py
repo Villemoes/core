8eb916bbe4d2424d80ddafa809e711418dc696ec
Rasmus Villemoes <rasmus.villemoes@prevas.dk>
2017-02-03 00:02:06 +0100

    oelite/fetch/sigfile.py: make it a little more robust
    I'm not sure this is really worth the complexity it adds, but it
    annoyed the correctness nerd in me.
    
    If
    
    - a .oe file has several recipe types,
    - we build at least two of them,
    - they happen to have a different set of SRC_URIs, and
    - we lack signatures for those
    
    each do_fetch will write a .sig file, but only containing the
    signatures pertaining to the SRC_URIs for that variant. The end result
    is a .sig file not containing all the signatures it should, and thus
    the next build is again going to fail with a "signatures changed,
    rebuild needed".
    
    We can avoid that by updating our view of the world before writing the
    new contents.
    
    Doing do_fetch in separate processes would make this a slightly bigger
    problem than just having to run "oe bake" n+1 times; without
    synchronization, we could end up generating corrupt .sig files. So
    while we're at it, prepare for that by throwing in flock(LOCK_EX).

--- lib/oelite/fetch/sigfile.py
+++ lib/oelite/fetch/sigfile.py
@@ -1,5 +1,6 @@
 from collections import MutableMapping
 import os
+import fcntl
 
 class SignatureFile(MutableMapping):
 
@@ -8,9 +9,8 @@ class SignatureFile(MutableMapping):
         self.signatures = {}
         if os.path.exists(filename):
             with open(self.filename, "r") as sigfile:
-                for sigline in sigfile:
-                    signature, localname = sigline.strip().split(None, 1)
-                    self.signatures[localname] = signature
+                fcntl.flock(sigfile.fileno(), fcntl.LOCK_EX)
+                self.read_lines(sigfile)
 
     def __getitem__(self, key): # required by Mapping
         return self.signatures[key]
@@ -29,10 +29,21 @@ class SignatureFile(MutableMapping):
     def __iter__(self): # required by Iterable
         return self.signatures.__iter__()
 
+    def read_lines(self, f):
+        for sigline in f:
+            signature, localname = sigline.strip().split(None, 1)
+            # Should we warn if a conflicting value for localname exists?
+            self.signatures[localname] = signature
+
     def write(self):
-        localnames = self.signatures.keys()
-        localnames.sort()
-        with open(self.filename, "w") as sigfile:
+        fd = os.open(self.filename, os.O_RDWR | os.O_CREAT, 0o666)
+        fcntl.flock(fd, fcntl.LOCK_EX)
+        with os.fdopen(fd, "r+") as sigfile:
+            self.read_lines(sigfile)
+            os.ftruncate(sigfile.fileno(), 0)
+            sigfile.seek(0)
+            localnames = self.signatures.keys()
+            localnames.sort()
             for localname in localnames:
                 sigfile.write("%s  %s\n"%(self.signatures[localname],
                                           localname))
